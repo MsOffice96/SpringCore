158af9ccc55041b1fcd3962399de6732788b7385
yangminsoo <yangminsoo21@gmail.com>
2024-05-19 21:42:22 +0900

    1->2

--- src/test/java/hello/core/a/aa.java
+++ src/test/java/hello/core/a/aa.java
@@ -1,31 +1,5 @@
 package hello.core.a;
 
 public class aa {
-    // notebook1 -> 3
-    // desktop1
-    // notebook2
-
-    // final desktop\
-
-    //hi
-
-    //hello
-
-    // 11
-
-    // 22
-
-    // 33
-    
-    // 44
-
-    // 2->1
-
     // 1->2
-
-    // 2->1
-
-    // 1->2
-
-    // 2->1
 }
